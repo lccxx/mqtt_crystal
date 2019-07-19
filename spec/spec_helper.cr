e90535676ba53c0f2fe15889ca6e5d3669e03187
Soumya <soumyasr@google.com>
2019-07-18 23:38:12 -0700

    [breaking] Use Blocking API for messages
    It's easy to convert blocking API to non-blocking via spawn and channels, but it's much harder to convert a callback-based API to be blocking, and since crystal makes it easy to avoid callback hell, providing a blocking API is the most flexible option.
    
    See also comments on the crystal-lang websocket API: https://github.com/crystal-lang/crystal/issues/5600#issuecomment-402553447

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,3 +8,34 @@ def slice_it(a : String) : Bytes
   bytes.map_with_index { |b, i| slice[i] = b }
   slice
 end
+
+def with_mosquitto
+  config = File.tempfile("mosquitto.conf")
+  config << "port 1883"
+  config << "allow_anonymous true"
+
+  ready = Channel(Bool).new
+  done = Channel(Bool).new
+  spawn do
+    Process.run("mosquitto", ["-c", config.path], input: Process::Redirect::Close) { |p|
+      loop {
+        break if p.error.read_line.includes? "on port 1883"
+      }
+      ready.send true
+      # Cleanup mosquitto after test finishes
+      done.receive
+      begin
+        p.kill
+      rescue
+      end
+    }
+  end
+
+  ready.receive
+  begin
+    yield
+  ensure
+    done.send true
+    config.delete
+  end
+end
